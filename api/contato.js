fb21ac56b5f957f6f389f894cbd00e283bb63135
Victor Alves <148339750+Eleuta1@users.noreply.github.com>
2026-05-23 12:52:05 -0300

    Update contato.js

--- api/contato.js
+++ api/contato.js
@@ -13,5 +13,5 @@ export default async function handler(req, res) {
   });
 
   // Redireciona para a URL privada
-  res.redirect(302, process.env.REDIRECT_URL);
+  return res.redirect(redirectTo);
 }
