4dcb2bf564f290f5681143a6ff31fa4960c9f4b6
Victor Alves <148339750+Eleuta1@users.noreply.github.com>
2026-05-23 13:14:14 -0300

    Update contato.js

--- api/contato.js
+++ api/contato.js
@@ -1,11 +1,11 @@
 import { parse } from 'querystring';
- 
+
 export const config = {
   api: {
     bodyParser: false,
   },
 };
- 
+
 async function getRawBody(req) {
   return new Promise((resolve, reject) => {
     let data = '';
@@ -14,20 +14,19 @@ async function getRawBody(req) {
     req.on('error', reject);
   });
 }
- 
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).end('Method Not Allowed');
   }
- 
+
   const raw = await getRawBody(req);
   const body = parse(raw);
- 
   const { nome_completo, email, telefone, mensagem } = body;
- 
+
   console.log('Dados recebidos:', { nome_completo, email, telefone });
- 
-  await fetch(`https://formsubmit.co/ajax/${process.env.FORM_EMAIL}`, {
+
+  await fetch('https://formsubmit.co/ajax/contato@picolabz.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -35,6 +34,6 @@ export default async function handler(req, res) {
     },
     body: JSON.stringify({ nome_completo, email, telefone, mensagem }),
   });
- 
+
   res.redirect('https://picolabz.com/pages/thanks.html');
 }
